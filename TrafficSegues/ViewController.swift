520123b4319e8b7615fd7a53951585d1c5ac3e97
Shaqra University <ccitlms@gmail.com>
2022-03-06 12:12:02 +0300

    View Controller Issue

--- TrafficSegues/ViewController.swift
+++ TrafficSegues/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+  //  @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue) {
+    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue) {
+        }
+        
 }
+    
 
